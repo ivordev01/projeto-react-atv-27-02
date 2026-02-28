0c50e239164345f540a35b0a1d48c4a15b3d92fa
ivordev01 <rosa.ivor1721@gmail.com>
2026-02-27 22:28:05 -0300

    Salvamento 1

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,53 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const [count, setCount] = useState(0);
+  const [signal, setSignal] = useState(false);
+  const [array, setArray] = useState<number[]>([]);
+  const [inputValue, setInputValue] = useState("");
+  const [inputList, setInputList] = useState<string[]>([]);
+
+  function appendInput() {
+      const novaTarefa = 
+      {
+        Tarefa: inputValue,
+        data: new Date()
+      }
+
+    setInputList([...inputList, inputValue]);
+    setInputValue("");
+  }
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "skyblue",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TextInput
+        value={inputValue}
+        onChangeText={setInputValue}
+        style={{
+          borderWidth: 1,
+          padding: 10,
+          marginBottom: 10,
+          borderRadius: 5,
+        }}
+      />
+      <Text style={{ padding: 10, fontSize: 30 }}>
+        {JSON.stringify(inputList, null, 2)}
+        
+      </Text>
+      <TouchableOpacity
+        onPress={appendInput}
+        style={{ padding: 20, backgroundColor: "black", borderRadius: 10 }}
+      >
+        <Text style={{ color: "white" }}>Clique em mim</Text>
+      </TouchableOpacity>
     </View>
   );
 }
